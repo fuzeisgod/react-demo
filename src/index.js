56bf265b8c2ec17def6bf810230971908cb32f43
fuzzy <17826806525@163.com>
2020-05-04 01:32:18 +0800

    组件中的样式

--- src/index.js
+++ src/index.js
@@ -1,39 +1,29 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import classNames from 'classnames'
+import styled from 'styled-components'
 
-// 定义组件的第二种方式，使用类继承 React.Component
-// class App extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 <h1>类组件！</h1>
-//                 <p>{this.props.title}</p>
-//             </div>
-//         )
-//     }
-// }
+import './index.css'
 
-// React.createElement 是一个方法，用于创建元素，可以有很多的参数，但是前两个是固定的：
-// 第一个可以理解为标签名
-// 第二个可以理解为标签的属性
-// 剩下的，可以继续写子元素
+const Title = styled.h1`
+    color: red;
+`
+
 class App extends Component {
     render() {
+        // 这里不是在jsx语法内，不需要外面再包一层花括号
+        const style = { color: 'red' }
         return (
-            React.createElement(
-                'div',
-                {},
-                React.createElement(
-                    'h1',
-                    {},
-                    '类组件！'
-                ),
-                React.createElement(
-                    'p',
-                    {},
-                    this.props.title
-                )
-            )
+            <div>
+                <Title>元素中的样式</Title>
+                <ul>
+                    {/* 第一层花括号表示要在jsx中写javascript语句，第二层花括号表示元素的样式对象 */}
+                    <li style={style}>使用style内联创建</li>
+                    <li className="has-text-red">使用class的方式，但是在react中class要写成className</li>
+                    <li className={classNames('a', { 'b': true, 'c': false })}>要动态添加不同的className，就可以使用第三方的包叫classNames, 比如这个li标签上就只有a,b两种className，没有c</li>
+                    <li>styled-components的使用</li>
+                </ul>
+            </div >
         )
     }
 }
