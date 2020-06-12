0a459ea4aa2878f203a5a770e467d984c0ec671c
fuzzy <17826806525@163.com>
2020-06-12 16:39:46 +0800

    6.12

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 
+import {
+    CartList
+} from './components'
+
 export default class App extends Component {
     render() {
         return (
             <div>
-                App
+                <CartList/>
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import App from './App'
+import store from './store'
+console.log(store)
 
 render(
     <App/>,
