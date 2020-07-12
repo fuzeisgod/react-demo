15a204e530127fe99d9be8bf0948a33b344276ca
fuzzy <17826806525@163.com>
2020-07-12 21:55:22 +0800

    ui框架 antd

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { Button } from 'antd';
+import './App.less'
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Button type="primary">Button</Button>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React from 'react'
-import ReactDom from 'react-dom'
+import { render } from 'react-dom'
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd'
+import App from './App'
 
-// const createApp = (props) => {
-//     return (
-//         // 在jsx语法中插入js的代码，要在花括号内
-//         <h1>hello {props.title}</h1>
-//     )
-// }
-
-// const app = createApp({
-//     title: 'React 16.13'
-// })
-
-
-// 创建组件的第一种方式：使用箭头函数，但是函数名称首字母要大写
-const App = (props) => {
-    return (
-        <h1 title={props.title}>hello {props.title}</h1>
-    )
-}
-
-ReactDom.render(
-    <App title="react" />,
+render(
+    <ConfigProvider locale={zhCN}>
+        <App />
+    </ConfigProvider>,
     document.querySelector('#root')
 )
