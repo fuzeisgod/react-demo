74c550b3769c70d331b5ce3abe1b50a05e5acbbd
fuzzy <17826806525@163.com>
2020-08-02 01:19:52 +0800

    8.2

--- src/index.js
+++ src/index.js
@@ -3,6 +3,56 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Map, List, fromJS } from 'immutable'
+
+
+
+const state = {
+  name: 'QF',
+  courses: ['a', 'b', 'c']
+}
+
+
+// 1. 关于对象的操作 Map
+const imState = Map({
+  name: 'QF',
+  courses: ['a', 'b', 'c']
+})
+
+console.log(state.name, imState.get('name'))
+// immutable 对象修改数据（数据发生改变）后会生成一个新对象，要用一个新参数接收
+const newImState = imState.set('name', 'czx')
+console.log(imState.get('name'), newImState.get('name'))
+
+
+
+// 2. 关于数组的操作 List
+const list = [1, 2]
+
+const imList = List([1, 2])
+const newImList = imList.push(3)
+
+console.log(imList.get(0), newImList.get(2))
+
+
+
+// 3. 关于复杂对象操作 fromJS
+const fromJSState = fromJS(state)
+console.log(fromJSState.get('courses').get(0))
+console.log(fromJSState.getIn(['courses', 0]))
+
+const newfromJSState = fromJSState.setIn(['courses', 0], 'A')
+console.log(newfromJSState.getIn(['courses', 0]))
+
+
+
+
+
+
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
